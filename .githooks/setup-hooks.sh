dd8a785b8c2b7a48af619f2cd2bed73abcdfb7e6
aba-d <abasaheb.dubal@gmail.com>
2025-11-04 20:44:09 -0600

    refactor(code): SCRUM-31 Updated code for githooks

--- .githooks/setup-hooks.sh
+++ .githooks/setup-hooks.sh
@@ -4,3 +4,5 @@
 git config core.hooksPath .githooks
 echo "Git hooks path set to .githooks"
 echo "✅ Pre-commit and commit-msg hooks are now active."
+echo "verify the git hooks configuratured or not by running the command below:"
+git config core.hooksPath
